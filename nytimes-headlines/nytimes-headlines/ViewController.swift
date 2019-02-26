42458f38c2217646fac1da3ae43f8fbd9649d797
Kutan Çıngısız <cingisiz.akutan@gmail.com>
2019-02-26 19:21:08 +0300

    New Xcode Project

---
+++ nytimes-headlines/nytimes-headlines/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nytimes-headlines
+//
+//  Created by KUTAN ÇINGISIZ on 26.02.2019.
+//  Copyright © 2019 KUTAN ÇINGISIZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
