eaa297cc47b431b550e9d2d2bb93051edcd2ac43
Kutan Çıngısız <cingisiz.akutan@gmail.com>
2019-03-03 19:25:52 +0300

    still cleanup

---
+++ nytimes-headlines/nytimes-headlines/Controllers/Headlines/Subview/HeadlinesRemaningArticleView.swift
@@ -0,0 +1,9 @@
+//
+//  HeadlinesRemaningArticleView.swift
+//  nytimes-headlines
+//
+//  Created by KUTAN ÇINGISIZ on 3.03.2019.
+//  Copyright © 2019 KUTAN ÇINGISIZ. All rights reserved.
+//
+
+import Foundation
---
+++ nytimes-headlines/nytimes-headlines/Controllers/Headlines/Subview/HeadlinesTitleView.swift
@@ -0,0 +1,9 @@
+//
+//  HeadlinesTitleView.swift
+//  nytimes-headlines
+//
+//  Created by KUTAN ÇINGISIZ on 3.03.2019.
+//  Copyright © 2019 KUTAN ÇINGISIZ. All rights reserved.
+//
+
+import Foundation
